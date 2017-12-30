ac5fb523737031cc47769d59387243a7637f6074
root <nopains@126.com>
2017-12-30 19:44:52 +0800

    [添加功能]实现store的单元测试

--- test/unit/specs/store/getData.spec.js
+++ test/unit/specs/store/getData.spec.js
@@ -1,9 +1,11 @@
 import {store} from '@/store'
 describe('store', () => {
-  it('should render correct contents', (done) => {
+  it('should render correct contents', (done) => { // 供异步调用
+    store.state.data = null
+      expect(store.state.data).toEqual(null) // 检查state状态
     store.dispatch('getData').then(() => {
-      expect(store.state.data.id).toEqual(1)
-      done()
+      expect(store.state.data.id).toEqual(1) // 检查state状态
+      done() // 异步触发断言的方式
     })
   })
 })
