c4ece2456ba3353f56afe45442505095624283aa
root <nopains@126.com>
2018-01-14 17:11:16 +0800

    添加嵌套解析ｄｅｍｏ

---
+++ src/components/Macro.vue
@@ -0,0 +1,24 @@
+<script>
+import $ from 'jquery'
+export default {
+  name: 'HelloWorld',
+  data: () => {
+    return {}
+  },
+  props: ['message'],
+  methods: {
+    showMessage (event) {
+      if ($(event.target).parents('.macro').length > 0) {
+        alert(this.message)
+        event.stopPropagation()
+        event.cancelBubble = true
+        return false
+      }
+      alert(this.message)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Nest.vue
@@ -0,0 +1,76 @@
+<template>
+  <div ref='container'>
+    <div v-pre class='macro macro1' v-on:click='showMessage($event)'>
+      <div v-pre class='macro macro2' v-on:click='showMessage($event)'>
+        macro2
+        <div v-pre class='macro macro3' v-on:click='showMessage($event)'>
+          macro3
+        </div>
+        <img data-props="macro3"></img>
+      </div>
+      <img data-props="macro2"></img>
+      macro1
+    </div>
+    <img data-props="macro1"></img>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+import _ from 'lodash'
+import Macro from './Macro'
+import Vue from 'vue'
+export default {
+  mounted () {
+    this.parse({
+      container: this.$refs.container
+    })
+  },
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    escapeParse ({
+      container
+    }) {
+      $(container).children().find('[v-on:click]').each((item) => {
+        let clickHandler = $(item).attr('v-on:click')
+        $(item).attr('_v-on:click', clickHandler)
+        $(item).removeAttr('v-on:click')
+      })
+    },
+    parse ({container}) {
+      _.reverse(Array.from($(container).find('img'))).forEach(function(element, index) {
+        if (index > 0) {
+          return false
+        }
+        let prev = $(element).prev()[0]
+        console.log(prev)
+        $(prev).removeAttr('v-pre')
+        // this.escapeParse({
+        //   container: prev
+        // })
+        let propsData = $(element).data('props')
+        console.log(propsData)
+        let constructor = Vue.extend(Macro)
+        let macroComponent = new constructor({
+          el: prev,
+          propsData: {
+            message: propsData
+          }
+        })
+        this.parse ({
+          container: macroComponent.$el
+        })
+      }, this)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
